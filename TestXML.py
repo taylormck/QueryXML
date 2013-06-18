269151085a0197685af5c5080fdafb7ae65706f0
Taylor McKinney <taylorsmck@gmail.com>
2013-06-18 14:53:44 -0500

    Set up the TestXML.py file but did not add unit tests

---
+++ TestXML.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# --- imports ---
+import StringIO
+import unittest
+from XML import XMLSearch, xmlPrint, xmlRead, xmlSolve
+
+class TestXML (unittest.TestCase):
+    # The function tests should go here
+    #       (xmlSolve, xmlRead, xmlPrint)
+    pass
+
+class TestXMLSearch (unittest.TestCase):
+    # The method tests for the xmlSearch class should go here
+    #       (__init__ and eval)
+    pass
+
+print "TestXML.py"
+unittest.main()
+print "Done."
