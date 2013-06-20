e3d5ceac21c83f2632c48c916286173fabf5d1bb
Eddie <saenzeddie5@gmail.com>
2013-06-19 23:48:08 -0500

    added xmlRead tests

--- TestXML.py
+++ TestXML.py
@@ -13,12 +13,48 @@ import unittest
 from XML import XMLSearch, xmlPrint, xmlRead, xmlSolve, et
 
 class TestXML (unittest.TestCase) :
+
     # -------
     # xmlRead
     # -------
 
+    # may have to remove \n from the assert statements
+
     def test_xmlRead (self) :
-        self.assert_(False)
+        r = StringIO.StringIO("<THU>\n<Team>\n<ACRush></ACRush>\n<Jelly></Jelly>\n<Cooly></Cooly>\n</Team>\n<JiaJia>\n<Team>\n<Ahyangyi></Ahyangyi>\n<Dragon></Dragon>\n<Cooly><Amber></Amber></Cooly>\n</Team>\n</JiaJia>\n</THU>\n<Team><Cooly></Cooly></Team>")
+        a = ["", ""]
+        b = xmlRead(r, a)
+        self.assert_(b == True)
+        self.assert_(a[0] == "<THU>\n<Team>\n<ACRush></ACRush>\n<Jelly></Jelly>\n<Cooly></Cooly>\n</Team>\n<JiaJia>\n<Team>\n<Ahyangyi></Ahyangyi>\n<Dragon></Dragon>\n<Cooly><Amber></Amber></Cooly>\n</Team>\n</JiaJia>\n</THU>")
+        self.assert_(a[1] == "<Team><Cooly></Cooly></Team>")
+
+    def test_xmlRead_empty (self) :
+        r = StringIO.StringIO("")
+        a = ["", ""]
+        b = xmlRead(r, a)
+        # the below may be bad because we allow blank lines in input
+        self.assert_(b == False)
+        self.assert_(a[0] == "")
+        self.assert_(a[1] == "")
+
+    def test_xmlRead_whitespace (self) :
+        r = StringIO.StringIO("<THU>\n <Team>  \n\n <Cooly>\t</Cooly>\n </Team>\n </THU>\n<Team><Cooly>\t </Cooly> </Team>")
+        a = ["", ""]
+        b = xmlRead(r, a)
+        self.assert_(b == True)
+        # xml preserves whitespace, do we want to preserve it? double check
+        self.assert_(a[0] == "<THU>\n<Team>\n<Cooly></Cooly>\n</Team>\n</THU>")
+        self.assert_(a[1] == "<Team><Cooly></Cooly></Team>")
+
+    def test_xmlRead_text (self) :
+        r = StringIO.StringIO("<THU>\n<Team>\n<Cooly>test text</Cooly>\n</Team>\n</THU>\n<Team><Cooly>more test text</Cooly></Team>")
+        a = ["", ""]
+        b = xmlRead(r, a)
+        self.assert_(b == True)
+        # xml preserves whitespace, so it might also preserve the text? double check
+        self.assert_(a[0] == "<THU>\n<Team>\n<Cooly></Cooly>\n</Team>\n</THU>")
+        self.assert_(a[1] == "<Team><Cooly></Cooly></Team>")
+
 
     # --------
     # xmlPrint
@@ -26,7 +62,7 @@ class TestXML (unittest.TestCase) :
 
     def test_xmlPrint_empty (self) :
         w = StringIO.StringIO()
-        ans = ()
+        ans = () # parens or empty tuple?
         xmlPrint(ans, w)
         self.assert_(w.getvalue() == "")
 
